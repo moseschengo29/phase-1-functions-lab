318cf4a5cd5b3319f7564fd3178a49ec9c68bc80
Moses <mochengo29@gmail.com>
2023-06-15 11:28:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -6,12 +6,27 @@ function distanceFromHqInBlocks(block) {
 
 function distanceFromHqInFeet(block) {
     const blockInFeet = 264; 
-    return Math.abs(block - distanceFromHq) * blockInFeet;
+    // return Math.abs(block - distanceFromHq) * blockInFeet;
+
+    if (distanceFromHq >= block){
+      return (distanceFromHq - block) * blockInFeet;
+    }
+
+    else if (block >= distanceFromHq){
+      return (block - distanceFromHq) * blockInFeet;
+    }
 }
 
 function distanceTravelledInFeet(startBlock, endBlock) {
     const blockInFeet = 264;
-    const distance = Math.abs(startBlock - endBlock) * blockInFeet;
+    let distance;
+    if(startBlock >= endBlock){
+      return (startBlock - endBlock) * blockInFeet;
+    }
+    else if(endBlock >= startBlock){
+      return (endBlock - startBlock) * blockInFeet;
+    }
+
     return distance;
 }
 
@@ -26,7 +41,7 @@ function calculatesFarePrice(start, destination) {
     } 
     else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
       const fare = (distanceInFeet - 400) * 0.02;
-      return fare 
+      return fare;
 
     } 
     else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
